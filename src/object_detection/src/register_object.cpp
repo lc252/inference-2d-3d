8c3e0a2b9fc8143d83035914ec40cdc542c6f21c
lc252 <lc252@uowmail.edu.au>
2023-02-15 11:38:21 +1100

    put processes into functions

--- src/object_detection/src/register_object.cpp
+++ src/object_detection/src/register_object.cpp
@@ -27,6 +27,7 @@
 // Types
 typedef pcl::PointNormal PointNT;
 typedef pcl::PointCloud<PointNT> PointCloudT;
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
 typedef pcl::FPFHSignature33 FeatureT;
 typedef pcl::FPFHEstimationOMP<PointNT, PointNT, FeatureT> FeatureEstimationT;
 typedef pcl::PointCloud<FeatureT> FeatureCloudT;
@@ -34,32 +35,81 @@ typedef pcl::PointCloud<FeatureT> FeatureCloudT;
 // Publishers
 ros::Publisher object_aligned_pub;
 
-float leaf_size;
-bool segment_plane;
-int MaximumIterations;
-int NumberOfSamples;
-int CorrespondenceRandomness;
-float SimilarityThreshold;
-float MaxCorrespondenceDistance;
-float InlierFraction;
 
 void get_parameters()
 {
-    ros::param::get("leaf_size", leaf_size);
-    ros::param::get("segment_plane", segment_plane);
-    ros::param::get("align_MaximumIterations", MaximumIterations);
-    ros::param::get("align_NumberOfSamples", NumberOfSamples);
-    ros::param::get("align_CorrespondenceRandomness", CorrespondenceRandomness);
-    ros::param::get("align_SimilarityThreshold", SimilarityThreshold);    
-    ros::param::get("align_MaxCorrespondenceDistance", MaxCorrespondenceDistance);    
-    ros::param::get("align_InlierFraction", InlierFraction);
+
 }
+
+void downsample(PointCloudT::Ptr &cloud)
+{
+    float leaf_size;
+    ros::param::get("leaf_size", leaf_size);
+    pcl::VoxelGrid<PointNT> grid;
+    grid.setLeafSize(leaf_size, leaf_size, leaf_size);
+    grid.setInputCloud(cloud);
+    grid.filter(*cloud);
+}
+
+void remove_plane(PointCloudT::Ptr &cloud)
+{
+    bool segment_plane;
+    ros::param::get("segment_plane", segment_plane);
+    if (!segment_plane)
+    {
+        return;
+    }
+
+    ROS_INFO("Segmenting");
+    pcl::SACSegmentation<PointNT> seg;
+    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+    pcl::ModelCoefficients::Ptr coeffs(new pcl::ModelCoefficients);
+    seg.setOptimizeCoefficients(false);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(100);
+    seg.setDistanceThreshold(0.0025);   // tested previously
+    seg.setInputCloud(cloud);
+    seg.segment(*inliers, *coeffs);
+    if (inliers->indices.size() != 0)
+    {
+        pcl::ExtractIndices<PointNT> extractor;
+        extractor.setInputCloud(cloud);
+        extractor.setIndices(inliers);
+        extractor.setNegative(true);        // keep NON planar
+        extractor.filter(*cloud);
+    }
+    else
+    {
+        ROS_WARN("No plane found, no segmentation...");
+    }
+    return;
+}
+
+void est_normals(PointCloudT::Ptr &cloud)
+{
+    float sr;
+    ros::param::get("normal_SearchRadius", sr);
+    pcl::NormalEstimationOMP<PointNT, PointNT> nest;
+    nest.setRadiusSearch(0.01);
+    nest.setInputCloud(cloud);
+    nest.compute(*cloud);
+}
+
+void est_features(PointCloudT::Ptr &cloud, FeatureCloudT::Ptr &features)
+{
+    float sr;
+    ros::param::get("features_SearchRadius", sr);
+    FeatureEstimationT fest;
+    fest.setRadiusSearch(0.025);
+    fest.setInputCloud(cloud);
+    fest.setInputNormals(cloud);
+    fest.compute(*features);
+}
+
 
 void register_object_cb(object_detection::Detection3D detection)
 {
-    // Get Latest Parameters
-    get_parameters();
-
     // Point clouds
     PointCloudT::Ptr object(new PointCloudT);
     PointCloudT::Ptr object_aligned(new PointCloudT);
@@ -71,64 +121,39 @@ void register_object_cb(object_detection::Detection3D detection)
     // load scene
     pcl::fromROSMsg(detection.cloud, *scene);
     // load object
-    // pcl::io::loadOBJFile<PointNT>("/home/fif/lc252/inference-2d-3d/src/object_detection/model_geometry/model_car_scaled.obj", *object);
+    // pcl::io::loadOBJFile<PointNT>("PATH/TO/.obj", *object);
     pcl::io::loadPCDFile<PointNT>("/home/fif/lc252/inference-2d-3d/src/object_detection/model_geometry/model_car_scaled_normal.pcd", *object);
 
+    ROS_INFO("Downsampling Clouds");
     // Downsample
-    pcl::VoxelGrid<PointNT> grid;
-    grid.setLeafSize(0.005, 0.005, 0.005);
-    grid.setInputCloud(object);
-    grid.filter(*object);
-    grid.setLeafSize(0.002, 0.002, 0.002);
-    grid.setInputCloud(scene);
-    grid.filter(*scene);
+    downsample(scene);
+    downsample(object);
 
-    // Segment and remove largest plane
-    ROS_INFO("Segmenting");
-    pcl::SACSegmentation<PointNT> seg;
-    pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
-    pcl::ModelCoefficients::Ptr coeffs(new pcl::ModelCoefficients);
-    seg.setOptimizeCoefficients(false);
-    seg.setModelType(pcl::SACMODEL_PLANE);
-    seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setMaxIterations(100);
-    seg.setDistanceThreshold(0.0025);   // tested previously
-    seg.setInputCloud(scene);
-    seg.segment(*inliers, *coeffs);
-    if (inliers->indices.size() != 0)
-    {
-        pcl::ExtractIndices<PointNT> extractor;
-        extractor.setInputCloud(scene);
-        extractor.setIndices(inliers);
-        extractor.setNegative(true);        // keep NON planar
-        extractor.filter(*scene);
-    }
-    else
-    {
-        ROS_WARN("No plane found, no segmentation...");
-    }
+    ROS_INFO("Removing Plane");
+    // segment and remove floor plane
+    remove_plane(scene);
 
     // Estimate normals for object and scene
     ROS_INFO("Estimating Normals");
-    pcl::NormalEstimationOMP<PointNT, PointNT> nest;
-    nest.setRadiusSearch(0.01);
-    nest.setInputCloud(object);
-    nest.compute(*object);
-    nest.setInputCloud(scene);
-    nest.compute(*scene);
+    est_normals(scene);
+    est_normals(object);
 
     // Estimate features
     ROS_INFO("Estimating Features Object");
-    FeatureEstimationT fest;
-    fest.setRadiusSearch(0.025);
-    fest.setInputCloud(object);
-    fest.setInputNormals(object);
-    fest.compute(*object_features);
+    est_features(object, object_features);
     ROS_INFO("Estimating Features Scene");
-    fest.setInputCloud(scene);
-    fest.setInputNormals(scene);
-    fest.compute(*scene_features);
-
+    est_features(scene, scene_features);
+    
+    // Get align parameters from ros
+    int MaximumIterations, NumberOfSamples,CorrespondenceRandomness;
+    float SimilarityThreshold, MaxCorrespondenceDistance, InlierFraction;
+    ros::param::get("align/MaximumIterations", MaximumIterations);
+    ros::param::get("align/NumberOfSamples", NumberOfSamples);
+    ros::param::get("align/CorrespondenceRandomness", CorrespondenceRandomness);
+    ros::param::get("align/SimilarityThreshold", SimilarityThreshold);    
+    ros::param::get("align/MaxCorrespondenceDistance", MaxCorrespondenceDistance);    
+    ros::param::get("align/InlierFraction", InlierFraction);
+        
     // Perform alignment
     ROS_INFO("Aligning");
     pcl::SampleConsensusPrerejective<PointNT, PointNT, FeatureT> align;
@@ -136,12 +161,12 @@ void register_object_cb(object_detection::Detection3D detection)
     align.setSourceFeatures(object_features);
     align.setInputTarget(scene);
     align.setTargetFeatures(scene_features);
-    align.setMaximumIterations(300000);               // Number of RANSAC iterations, increase to tradeoff speed for accuracy (50000)
-    align.setNumberOfSamples(3);                     // Number of points to sample for generating/prerejecting a pose, increase to tradeoff speed for accuracy (3)
-    align.setCorrespondenceRandomness(5);            // Number of nearest features to use, increase to tradeoff speed for accuracy (5)
-    align.setSimilarityThreshold(0.5f);              // Polygonal edge length similarity threshold, decrease to tradeoff speed for accuracy (0.9)
-    align.setMaxCorrespondenceDistance(2.5f * 0.005); // Inlier threshold (2.5 * leaf size)
-    align.setInlierFraction(0.7f);                  // Required inlier fraction for accepting a pose hypothesis, increase  (0.25)
+    align.setMaximumIterations(MaximumIterations);               // Number of RANSAC iterations, increase to tradeoff speed for accuracy (50000)
+    align.setNumberOfSamples(NumberOfSamples);                     // Number of points to sample for generating/prerejecting a pose, increase to tradeoff speed for accuracy (3)
+    align.setCorrespondenceRandomness(CorrespondenceRandomness);            // Number of nearest features to use, increase to tradeoff speed for accuracy (5)
+    align.setSimilarityThreshold(SimilarityThreshold);              // Polygonal edge length similarity threshold, decrease to tradeoff speed for accuracy (0.9)
+    align.setMaxCorrespondenceDistance(MaxCorrespondenceDistance); // Inlier threshold (2.5 * leaf size)
+    align.setInlierFraction(InlierFraction);                  // Required inlier fraction for accepting a pose hypothesis, increase  (0.25)
     align.align(*object_aligned);
 
     if (!align.hasConverged())
